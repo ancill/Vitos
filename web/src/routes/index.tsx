9f308b00bf1ee974d3a3e3f321200894ce91bfaa
Ivan Arsenev <42762155+ancill@users.noreply.github.com>
2024-11-19 19:19:50 -0300

    eslint fix

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,24 +1,13 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "../components/ui/button";
-import { client } from "@/lib/api";
-import { useQuery } from "@tanstack/react-query";
 
 export const Route = createFileRoute("/")({
   component: Index,
 });
 
 function Index() {
-  const { data, refetch } = useQuery({
-    queryKey: ["hello"],
-    queryFn: () => client.api.hello.$get().then((response) => response.text()),
-    enabled: false,
-  });
-
   return (
     <div>
       <h2 className="text-xl mb-4">Welcome to VITOS</h2>
-      <Button onClick={() => refetch()}>Fetch Hello</Button>
-      {data && <p className="mt-4">{data}</p>}
     </div>
   );
 }
